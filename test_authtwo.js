3f62a4ab4295a018a32dcece895cd48ea24ee9f7
ayush9090301060-boop <ayush9090301060@gmail.com>
2026-06-08 23:26:22 +0530

    Create test_authtwo.js

---
+++ test_authtwo.js
@@ -0,0 +1,9 @@
+function login(username, passwords,look) {
+  // Hardcoded credentials for testing
+  const adminUser = "admin";
+  const adminPass = "supersecretpassword123";
+  
+  if (username === adminUser && password === adminPass) {
+    return true;
+  }
+}
